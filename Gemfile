f2bdd65f7c3c11fec3b2a8803abdbdfc7b089433
eleonore dedeyan <eleonore.dedeyan@gmail.com>
2016-11-09 16:46:46 +0000

    yelp

--- Gemfile
+++ Gemfile
@@ -26,7 +26,14 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem "better_errors"
+gem "binding_of_caller"
+gem "simple_form"
+gem 'rails-controller-testing'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+square = Restaurant.create(name: 'The Square', category: "french", address: "6-10 Bruton Street, Mayfair, London, W1J 6PU", phone_number: " +44 (0) 20 7495 7100" )
+sketch = Restaurant.create(name: 'Sketch', category: 'french', address: '9 Conduit Street, London, W1S 2XG')
+hakkasan = Restaurant.create(name: 'Hakkasan', category: 'chinese', address: '8 Hanway Place, London, W1T 1HD')
+river = Restaurant.create(name: 'The river cafe', category: 'italian', address: 'Thames Wharf, Rainville Road, London, W6 9HA')
+oka = Restaurant.create(name: 'Oka', category: 'japanese', address: 'Kingly Street, First Floor of Kingly Court, London, W1B 5PW', phone_number: '+44 20 7734 3556')
